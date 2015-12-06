8d846cb56eecc8ab688ce0edf9010a27822d2269
MOSOC <warshightide@gmail.com>
2015-12-06 16:48:55 +0000

    AWS-S3-2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,12 +5,12 @@ CarrierWave.configure do |config|
       provider:              'AWS',                        
       aws_access_key_id:     ENV["AWS_KEY_ID"],      # 你的 key           
 
-      aws_secret_access_key: ENV["AWS_SECRET_KEY"],      # 你的 secret key         
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],      # 你的 secret key   
 
-      region:                'eu-west-1' # 你的 S3 bucket 的 Region 位置   
+      region:                'ap-northeast-1' # 你的 S3 bucket 的 Region 位置   
 
     }
-    config.fog_directory  = 'xxxx' # 你設定的 bucket name 
+    config.fog_directory  = 'fubucket' # 你設定的 bucket name 
 
   else
     config.storage :file
