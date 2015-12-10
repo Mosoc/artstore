ccb7593020f61c780bb2e3e7b1331f917a6ee48b
MOSOC <warshightide@gmail.com>
2015-12-10 09:49:18 +0000

    AWS-2

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,11 +34,12 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-set :linked_files, %w{
-  config/database.yml
-}
+#set :linked_files, %w{
+#  config/database.yml
+#}
 
 set :linked_files, %w{
+  config/database.yml,
   config/application.yml
 }
 
